2ec8596d0e096f9cfe54b58ee642a08917a485d4
QuiteAir <hfvbkmr@mail.ru>
2020-10-03 23:37:49 +0300

    init: start and finish typical-arrays-problems-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length !== 0) {
+    let mini = array[0];
+    for (let i = 0; i < array.length - 1; i++) {
+      if (mini > array[i]) {mini = array[i];}
+    }
+    return mini;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length !== 0) {
+    let maxi = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (maxi < array[i]) {maxi = array[i];}
+    }
+    return maxi;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length !== 0) {
+    let avrg = array[0];
+    for (let i = 1; i < array.length; i++) {
+      avrg += array[i];
+    }
+    return avrg / array.length;
+  }
   return 0;
 }
